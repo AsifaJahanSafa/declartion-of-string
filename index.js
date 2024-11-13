b1b6d5a355cc995ba6d09703a6ad078ad08c615b
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-13 21:23:49 +0600

    first commit

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const country='Bangladesh';
 const profession="Developer";
 const city=`Dhaka`;
 const university=new String('Lalmatiya Mohila College');
-console.log(country)
+console.log(country);
+console.log(typeof country);
+console.log(profession);
+console.log(typeof profession);
+console.log(city);
+console.log(typeof city);
+console.log(university);
+console.log(typeof university)

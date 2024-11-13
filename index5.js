d84ee5ade7037b4de312f6492b5cc7b1696c4e9b
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-14 02:42:18 +0600

    aded object

---
+++ index5.js
@@ -0,0 +1,25 @@
+// ------------------------------------object is premitive(strings, numbers, and booleans, while more complex data structures like arrays, objects, )----------------------------------------------------------------
+
+const watch={
+    brand:'apple',
+    price:45000,
+    isNew:true,
+    color:'white'
+}
+
+console.log(watch)
+
+
+const subject={
+    name:'Chemistry',
+    teacher:'Jakia Mam',
+    date:'15 Nov 2024',
+    chapter:['First','Second','Third','Fifth'],
+    exam:{
+        type:'final',
+        question:15,
+        marks:100
+    }
+
+}
+console.log(subject)

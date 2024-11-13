cd398456bbf7cd2f8afa16a24655521bf9432d36
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-13 23:52:55 +0600

    added mix array

---
+++ index2.js
@@ -0,0 +1,13 @@
+// --------------------------------------Mix string---------------------------------------------------
+const university='Lal maTiya GoverNmenT MoHIla CollAGE';
+console.log(university);
+
+// /for uppercase
+
+console.log(university.toUpperCase());
+
+// /for lowercase
+
+console.log(university.toLowerCase())
+
+// conditonal with mix case

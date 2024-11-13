4dcf7bba17e31487d82e6671ea80f2ef51298ab3
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-14 00:05:24 +0600

    added trim

--- index2.js
+++ index2.js
@@ -10,4 +10,26 @@ console.log(university.toUpperCase());
 
 console.log(university.toLowerCase())
 
-// conditonal with mix case
+// conditionals with mix case
+
+const subject='Chemistry';
+const book='chemistry';
+
+if( subject.toLowerCase() === book.toLowerCase() ){
+    console.log('I study  Chemistry')
+}else{
+    console.log('what is chemistry')
+}
+
+
+
+// ---------------------------------------trim---------------------------------------------------------
+
+const drink=' water';
+const liquid='         water';
+
+if(drink.trim === liquid.trim){
+    console.log('I drink it')
+}else{
+    console.log('this is dirty')
+}

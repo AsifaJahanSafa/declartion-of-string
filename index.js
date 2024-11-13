e52bf967239d2f4a302bdb3dbb9dfa5d8259afb3
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-13 21:52:00 +0600

    added array characteristics

--- index.js
+++ index.js
@@ -9,4 +9,28 @@ console.log(typeof profession);
 console.log(city);
 console.log(typeof city);
 console.log(university);
-console.log(typeof university)
+console.log(typeof university);
+
+
+// Array is mutable(changable)
+
+let numbers=[12,34,45,32];
+console.log(numbers);
+
+// get array element index number
+console.log(numbers.length)
+
+// get array element by index
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3])
+
+
+// Change array element by its index"
+
+numbers[0]=78;
+console.log(numbers)
+
+// that's why array is mutable
